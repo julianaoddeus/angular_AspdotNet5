85834517e84a2229099463894d02aaa38407a36f
Juliana <64862169+julianaoddeus@users.noreply.github.com>
2022-09-28 22:01:26 -0300

    criando grid

--- proeventos/FRONT/proeventos/src/app/app.module.ts
+++ proeventos/FRONT/proeventos/src/app/app.module.ts
@@ -1,25 +1,32 @@
-import { ServiceEventos } from './eventos/service-eventos.service';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AppRoutingModule } from './app-routing.module';
 import { EventosComponent } from './eventos/eventos.component';
+import { ServiceEventos } from './eventos/service-eventos.service';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
       EventosComponent,
-      PalestrantesComponent
+      PalestrantesComponent,
+      NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+
   ],
   providers: [ServiceEventos],
   bootstrap: [AppComponent]
